ae6660fe92cdff3b641d75489217c451221bb645
Marko <tmarkec@gmail.com>
2023-10-03 13:36:01 +0100

    added functionality to 4th task

--- script.js
+++ script.js
@@ -28,8 +28,30 @@ closeBtn.addEventListener("click", function () {
 
     // Create a new heading element for the "Thank you" message
     const thankYouHeading = document.createElement("h1");
-    thankYouHeading.textContent = "Thank you for using the Tipulator";
+    thankYouHeading.textContent = "Thank you Marko";
 
     // Append the "Thank you" message to the body
     document.body.appendChild(thankYouHeading);
+});
+
+// Get the form and result elements
+const conversionForm = document.getElementById("conversion-form");
+const poundsAmountInput = document.querySelector("input[name='pounds-amount']");
+const resultElement = document.getElementById("result");
+
+// Add a submit event listener to the form
+conversionForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form submission
+
+    // Get the amount in pounds from the input field
+    const poundsAmount = parseFloat(poundsAmountInput.value);
+
+    // Check if the input is a valid number
+ 
+        // Perform the conversion (1 pound = 1.15 euros)
+        const eurosAmount = poundsAmount * 1.15;
+
+        // Display the result
+        resultElement.textContent = `${poundsAmount} pounds is equal to ${eurosAmount.toFixed(2)} euros.`;
+    
 });
