39bb5a5bb7d17a4d5f5940149825b5bf70c98d2a
Marko <tmarkec@gmail.com>
2023-10-03 19:13:51 +0100

    final change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//task1
 function setDarkMode() {
     let body = document.getElementById("heading");
     body.style = "background-color: black; color: white;"
@@ -8,6 +10,7 @@ function setLightMode() {
     body.style = "background-color: white; color: black;"
 }
 
+//task3
 function showMore() {
     let post = document.getElementById("answer");
     post.innerHTML = `
@@ -15,43 +18,23 @@ function showMore() {
         `
 }
 
-function changeTypeEmail() {
-    document.getElementById("login").type = "email";
-}
-
-const closeBtn = document.getElementById("close-btn");
-
-// Add a click event listener to the "Close" button
+//task 5
 closeBtn.addEventListener("click", function () {
-    // Clear the page content
-    document.body.innerHTML = "";
-
-    // Create a new heading element for the "Thank you" message
-    const thankYouHeading = document.createElement("h1");
-    thankYouHeading.textContent = "Thank you Marko";
-
-    // Append the "Thank you" message to the body
-    document.body.appendChild(thankYouHeading);
+    document.body.innerHTML = "<h1>Thank you Marko</h1>";
 });
 
-// Get the form and result elements
+// task 4
 const conversionForm = document.getElementById("conversion-form");
 const poundsAmountInput = document.querySelector("input[name='pounds-amount']");
 const resultElement = document.getElementById("result");
 
-// Add a submit event listener to the form
 conversionForm.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent form submission
 
-    // Get the amount in pounds from the input field
     const poundsAmount = parseFloat(poundsAmountInput.value);
 
-    // Check if the input is a valid number
- 
-        // Perform the conversion (1 pound = 1.15 euros)
-        const eurosAmount = poundsAmount * 1.15;
+    const eurosAmount = poundsAmount * 1.15;
 
-        // Display the result
-        resultElement.textContent = `${poundsAmount} pounds is equal to ${eurosAmount.toFixed(2)} euros.`;
+    resultElement.textContent = `${poundsAmount} pounds is equal to ${eurosAmount.toFixed(2)} euros.`;
     
 });
